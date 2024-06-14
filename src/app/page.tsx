ac41ff92e02d86244bb91f5d7d8b4ed5dd33cc30
Pablo-Arce-Jemersoft <parce@jemersoft.com>
2024-06-14 15:23:23 -0300

    feature: se agregan qs y type/qs para queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import { Button } from "@/components/ui/button";
+import qs from "qs";
 
-export default function Home() {
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+const getStrapiData = async (path: string) => {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  console.log(url.href)
+
+  try {
+    const response = await fetch(url.href);
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  const { title, description } = strapiData.data.attributes;
   return (
     <main className="containter mx-auto py-6">
-      <Button>Our Cool Button</Button>
+      <h1>{title}</h1>
+      <p>{description}</p>
     </main>
   );
 }
